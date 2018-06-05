04821b3a0218e36e3dfbc6a6ea88327393d8e3c6
wu <1635906091@qq.com>
2018-06-05 09:18:52 +0800

    frist commit

---
+++ src/com/itheima/bean/User.java
@@ -0,0 +1,23 @@
+package com.itheima.bean;
+
+import java.util.Date;
+
+public class User {
+
+
+    private int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args){
+        Date date = new Date();
+        System.out.println("今天不错");
+
+    }
+}
+
+
+
+
